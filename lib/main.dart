866e121e80ef821b90d378da1cf8db14d4999a91
edewajosh <joshedewa@gmail.com>
2020-10-13 20:56:15 +0300

    row

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,22 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      // body: Container(
-      //   padding: EdgeInsets.all(20.0),
-      //   margin: EdgeInsets.all(30.0),
-      //   color: Colors.grey[600],
-      //   child: Text('Hello'),
-      //
-      // ),
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('Hello'),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Hello'),
+          FlatButton(
+              onPressed: (){},
+              color: Colors.amber,
+              child: Text('Hello, World!')
+          ),
+          Container(
+            color: Colors.black26,
+            padding: EdgeInsets.all(30.0),
+            child: Text('Clueless'),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
